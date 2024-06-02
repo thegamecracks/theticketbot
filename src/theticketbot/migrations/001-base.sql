d7f307dc3aec2144e1379a939fe81d7803f14ad7
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-06-02 13:33:39 -0400

    feat: add initial work-in-progress schema
    Since we haven't deployed yet, I plan to continue editing this in-place
    to avoid creating too many migrations. This will result in breaking changes.

---
+++ src/theticketbot/migrations/001-base.sql
@@ -0,0 +1,65 @@
+CREATE TABLE user (id INTEGER PRIMARY KEY);
+CREATE TABLE guild (id INTEGER PRIMARY KEY);
+
+CREATE TABLE member (
+    guild_id
+        INTEGER
+        REFERENCES guild (id) ON DELETE CASCADE,
+    user_id
+        INTEGER
+        REFERENCES user (id) ON DELETE CASCADE,
+    PRIMARY KEY (guild_id, user_id)
+);
+
+CREATE TABLE channel (
+    id INTEGER PRIMARY KEY,
+    guild_id
+        INTEGER
+        REFERENCES guild (id) ON DELETE CASCADE
+);
+
+CREATE TABLE message (
+    id INTEGER PRIMARY KEY,
+    channel_id
+        INTEGER NOT NULL
+        REFERENCES channel (id) ON DELETE CASCADE
+);
+
+CREATE TABLE inbox (
+    id
+        INTEGER PRIMARY KEY
+        REFERENCES message (id) ON DELETE CASCADE,
+    starter_content TEXT,
+    max_tickets_per_user INTEGER NOT NULL DEFAULT 1
+);
+
+CREATE TABLE inbox_staff (
+    inbox_id
+        INTEGER
+        REFERENCES inbox (id) ON DELETE CASCADE,
+    mention TEXT NOT NULL, -- CHECK mention REGEXP '<@\d+>|<@&\d+>'
+    PRIMARY KEY (inbox_id, mention)
+);
+
+CREATE TABLE ticket (
+    id
+        INTEGER PRIMARY KEY
+        REFERENCES channel (id) ON DELETE CASCADE,
+
+    inbox_id INTEGER NOT NULL REFERENCES inbox (id),
+    owner_id INTEGER NOT NULL REFERENCES user (id)
+    -- Tickets don't disappear if their inbox message or owner are deleted,
+    -- so no cascade needed
+
+    -- The following invariants are not checked:
+    --   * channel.guild == inbox.message.channel.guild
+    --   * owner has a corresponding member row
+);
+
+-- Optimize counting a user's tickets in an inbox
+CREATE INDEX ix_ticket_inbox_owner ON ticket (inbox_id, owner_id);
+
+-- Optimize cascading foreign keys
+CREATE INDEX ix_member_user ON member (user_id);
+CREATE INDEX ix_channel_guild ON channel (guild_id);
+CREATE INDEX ix_message_channel ON message (channel_id);
