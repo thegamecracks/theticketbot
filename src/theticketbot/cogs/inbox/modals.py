e110f5723582b503f5b6dfcc92932b865655783b
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-01-05 15:58:43 -0500

    feat: migrate text input labels to components v2

--- src/theticketbot/cogs/inbox/modals.py
+++ src/theticketbot/cogs/inbox/modals.py
@@ -10,11 +10,13 @@ from .constants import DEFAULT_STARTER_CONTENT, DEFAULT_TICKET_NAME
 
 
 class SetInboxStarterContentModal(discord.ui.Modal, title="Starter Message"):
-    content = discord.ui.TextInput(
-        label="Content",
-        style=discord.TextStyle.long,
-        max_length=2000,
-        required=False,
+    content = discord.ui.Label(
+        text="Content",
+        component=discord.ui.TextInput(
+            style=discord.TextStyle.long,
+            max_length=2000,
+            required=False,
+        ),
     )
 
     def __init__(self, bot: Bot, inbox: discord.Message) -> None:
@@ -27,18 +29,23 @@ class SetInboxStarterContentModal(discord.ui.Modal, title="Starter Message"):
             return await translate(s, self.bot, locale=locale)
 
         self.title = await t(_("modal-starter"))
-        self.content.label = await t(_("modal-starter.content"))
+        self.content.text = await t(_("modal-starter.content"))
 
     async def set_defaults(self, conn: asqlite.Connection) -> None:
+        assert isinstance(self.content.component, discord.ui.TextInput)
+
         query = DatabaseClient(conn)
         starter_content = await query.get_inbox_starter_content(self.inbox.id)
         starter_content = starter_content or DEFAULT_STARTER_CONTENT
-        self.content.default = starter_content
+        self.content.component.default = starter_content
 
     async def on_submit(self, interaction: discord.Interaction):
+        assert isinstance(self.content.component, discord.ui.TextInput)
+        content = self.content.component
+
         async with self.bot.acquire() as conn:
             query = DatabaseClient(conn)
-            await query.set_inbox_starter_content(self.inbox.id, self.content.value)
+            await query.set_inbox_starter_content(self.inbox.id, content.value)
 
         content = await translate(
             _("modal-starter-finished"),
@@ -49,7 +56,10 @@ class SetInboxStarterContentModal(discord.ui.Modal, title="Starter Message"):
 
 
 class SetTicketDefaultsModal(discord.ui.Modal, title="New Tickets"):
-    name = discord.ui.TextInput(label="Name", max_length=100, required=False)
+    name = discord.ui.Label(
+        text="Name",
+        component=discord.ui.TextInput(max_length=100, required=False),
+    )
 
     def __init__(self, bot: Bot, inbox: discord.Message) -> None:
         super().__init__()
@@ -61,18 +71,23 @@ class SetTicketDefaultsModal(discord.ui.Modal, title="New Tickets"):
             return await translate(s, self.bot, locale=locale)
 
         self.title = await t(_("modal-new-tickets"))
-        self.name.label = await t(_("modal-new-tickets.name"))
+        self.name.text = await t(_("modal-new-tickets.name"))
 
     async def set_defaults(self, conn: asqlite.Connection) -> None:
+        assert isinstance(self.name.component, discord.ui.TextInput)
+
         query = DatabaseClient(conn)
         ticket_name = await query.get_inbox_default_ticket_name(self.inbox.id)
         ticket_name = ticket_name or DEFAULT_TICKET_NAME
-        self.name.default = ticket_name
+        self.name.component.default = ticket_name
 
     async def on_submit(self, interaction: discord.Interaction):
+        assert isinstance(self.name.component, discord.ui.TextInput)
+        name = self.name.component
+
         async with self.bot.acquire() as conn:
             query = DatabaseClient(conn)
-            await query.set_inbox_default_ticket_name(self.inbox.id, self.name.value)
+            await query.set_inbox_default_ticket_name(self.inbox.id, name.value)
 
         content = await translate(
             _("inbox-new-tickets-finished"),
